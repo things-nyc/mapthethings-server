1ac54d4a5b56a6a37701255e8c44c89e321ec6c0
Frank Leon Rose <frankleonrose@gmail.com>
2016-10-06 09:17:16 -0400

    Updated dependency versions - all tests pass

--- project.clj
+++ project.clj
@@ -5,37 +5,45 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   ;:repositories {"local" "file:${project.basedir}/local_repo"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.391"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/core.cache "0.6.5"]
                  [ch.hsr/geohash "1.3.0"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                 [compojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-defaults "0.2.0"]
-                 [environ "1.0.3"]
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
+                 [compojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.5.0"]
+                 [ring/ring-defaults "0.2.1"]
+                 [environ "1.1.0"]
                  [org.clojure/data.codec "0.1.0"]
                  ;;ClojureScript
-                 [org.clojure/clojurescript "1.7.122"]
-                 [cljs-ajax "0.5.1"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [cljs-ajax "0.5.8"]
                  [prismatic/dommy "1.1.0"]
                  [clojurewerkz/machine_head "1.0.0-beta9"]
-                 [amazonica "0.3.57"]
-                 [clj-time "0.11.0"]
+
+                 ; Assert Jackson common versions (brought in by Amazonica & cljs-ajax)
+                 [com.fasterxml.jackson.core/jackson-core "2.7.5"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.7.0"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.7.8"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.7.8"]
+                 [com.fasterxml.jackson.dataformat/jackson-dataformat-smile "2.7.8"]
+
+                 [amazonica "0.3.76"]
+                 [clj-time "0.12.0"]]
                  ;[com.amazonaws.geo/dynamodb-geo "1.0.0"]
 
   :min-lein-version "2.0.0"
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :plugins [
-    [lein-environ "1.0.3"]
-    [lein-figwheel "0.4.1"]
-    ]
+            [lein-environ "1.1.0"]
+            [lein-figwheel "0.5.8"]]
+
   :hooks [lein-environ.plugin/hooks]
   :uberjar-name "mapthethings-server-standalone.jar"
   :main mapthethings-server.web
@@ -50,8 +58,12 @@
                                     :output-dir "resources/public/js/out"}}]}
 
   :profiles {
-    :uberjar {:aot :all}
-    :production {:env {:production true}}
-    :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                          [ring/ring-mock "0.3.0"]
-                          ]}})
+             :uberjar {:aot :all}
+             :production {:env {:production true}}
+             :profiles {:dev {:source-paths ["dev"]
+                                :dependencies []}}
+             :dev {:source-paths ["dev"]
+                   :dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/java.classpath "0.2.3"]]}})
